096d243c15d43090e628e1c42bdcdeae258b9b85
Kerry Lang <klang812@gmail.com>
2021-02-05 10:15:57 -0800

    create test for mercuryAge

--- __tests__/solarAge.test.js
+++ __tests__/solarAge.test.js
@@ -1,15 +1,21 @@
 import { Age } from '../src/js/solarAge';
 
 describe ('Age', () => {
-  let personAge;
+  let earthAge;
   beforeEach(() => {
-    personAge = new Age(47);
+    earthAge = new Age(47);
   });
 
   test('should construct object with properties entered', () => {
     expect(personAge.age).toEqual(47);
   });
+
+  test('should calculate person age on Mercury')
+  let personAge = solarAge();  
+  expect(solarAge()).toEqual(personAge)
   
 });
 
 
+
+  
